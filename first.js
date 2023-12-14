0fac0203ec69efbb8f16ac1eae942d46e8cecf8b
Taufeeq Ahmed <144577017+TaufeeqAhmed123@users.noreply.github.com>
2023-12-14 00:17:51 -0800

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+for(let a=0; a<10 ;a++){
+    console.log(a,"hi ")
+
+}
+let guest=["aki","adnan","mehboob","sohel"]
+for(let s=0; s<guest.length;s++){
+    if (guest[s].length < 6){
+    console.log(`Mr.${guest[s]} you are invited to please come and join the welcome party`)
+    }
+}
